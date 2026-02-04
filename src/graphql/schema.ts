ec57202cd0b3d394339d7dfed93085c122e73e5b
Diogo Felipe <diogofelipe.dev@gmail.com>
2026-02-04 12:47:24 -0300

    update: root query, mutation

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,12 +1,18 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
 import { userQueries } from "./queries/user.queries";
 import { userMutations } from "./mutations/user.mutations";
+import { postQueries } from "./queries/post.queries";
+import { commentQueries } from "./queries/comment.queries";
+import { postMutations } from "./mutations/post.mutations";
+import { commentMutations } from "./mutations/comment.mutations";
 
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     ...userQueries,
+    ...postQueries,
+    ...commentQueries,
   }
 });
 
@@ -14,6 +20,8 @@ const RootMutation = new GraphQLObjectType({
   name: "RootMutationType",
   fields: {
     ...userMutations,
+    ...postMutations,
+    ...commentMutations
   }
 });
 
